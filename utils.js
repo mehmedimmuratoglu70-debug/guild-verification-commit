b7164de443aea8007f506c7ac8e96d4a148c3b59
mehmedimmuratoglu70-debug <mehmedimmuratoglu70@gmail.com>
2026-03-10 02:38:23 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T23:37:58.298Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-09T23:38:22.634Z
+Commit: Resolve null pointer exception in user service
