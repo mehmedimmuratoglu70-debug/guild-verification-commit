ed838baa6f4d77c82ff135df943dce77409c6226
mehmedimmuratoglu70-debug <mehmedimmuratoglu70@gmail.com>
2026-03-10 02:38:20 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T23:37:54.090Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-09T23:38:19.740Z
+Commit: Fix CSS styling bug on mobile devices
