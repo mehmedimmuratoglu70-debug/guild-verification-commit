a5647024ec6e58d753073dfaaf5e0b896b590231
mehmedimmuratoglu70-debug <mehmedimmuratoglu70@gmail.com>
2026-03-10 02:39:56 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T23:38:11.425Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-09T23:39:55.648Z
+Commit: Patch API endpoint error handling
