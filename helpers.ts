73aa8c869b3225a3faf710d186c70e8ae398bcc3
mehmedimmuratoglu70-debug <mehmedimmuratoglu70@gmail.com>
2026-03-10 02:42:20 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T23:41:52.256Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-09T23:42:20.064Z
+Commit: Patch API endpoint error handling
