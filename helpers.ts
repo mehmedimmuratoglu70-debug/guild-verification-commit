9ed022ce0e171a32378aae3f4494af374df9f604
mehmedimmuratoglu70-debug <mehmedimmuratoglu70@gmail.com>
2026-03-10 02:41:52 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T23:41:33.721Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-09T23:41:52.256Z
+Commit: Fix broken links in documentation
