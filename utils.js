7cb30a8a9da28177fc53a533245228e2bb5d7311
mehmedimmuratoglu70-debug <mehmedimmuratoglu70@gmail.com>
2026-03-10 02:39:17 +0300

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T23:38:22.634Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-09T23:39:17.251Z
+Commit: Introduce real-time notifications
