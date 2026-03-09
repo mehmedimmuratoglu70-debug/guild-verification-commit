433b2406dfe2e1e1d88dc0465d8a9e3ae0ea0a71
mehmedimmuratoglu70-debug <mehmedimmuratoglu70@gmail.com>
2026-03-10 02:41:34 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T23:39:55.648Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-09T23:41:33.721Z
+Commit: Patch security vulnerability in authentication
